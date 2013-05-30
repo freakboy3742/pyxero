f1b77db253213a82fdad2939317a253d85eb070f
Russell Keith-Magee <russell@keith-magee.com>
2013-05-30 15:38:48 +0800

    Fixed #2 -- Make sure setup.py will work before dateutil is installed.
    Thanks to stefanhenze for the report.

--- xero/__init__.py
+++ xero/__init__.py
@@ -1,4 +1,10 @@
-from .api import Xero
+try:
+    # This try-catch is necessary to make sure we can get
+    # VERSION from the base xero module before all the
+    # dependencies have been imported.
+    from .api import Xero
+except ImportError:
+    pass
 
-NUM_VERSION = (0, 5, 0)
+NUM_VERSION = (0, 5, 1)
 VERSION = ".".join(str(nv) for nv in NUM_VERSION)
