6dff95cd0fb08ef20bf85f54357ba0447d6975d8
Jake Morrison <jake@cogini.com>
2014-09-20 17:50:57 +0800

    Add support for Users, Receipts and ExpenseClaims

--- xero/api.py
+++ xero/api.py
@@ -7,7 +7,9 @@ class Xero(object):
     OBJECT_LIST = (u'Contacts', u'Accounts', u'CreditNotes',
                    u'Currencies', u'Invoices', u'Items', u'Organisation',
                    u'Payments', u'TaxRates', u'TrackingCategories', u'ManualJournals',
-                   u'BankTransactions', u'BankTransfers')
+                   u'BankTransactions', u'BankTransfers', 
+                   u'Users', u'Receipts', u'ExpenseClaims',
+                   )
 
     def __init__(self, credentials):
         # Iterate through the list of objects we support, for
