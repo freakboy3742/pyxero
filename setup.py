0309162fb33c1e8c407283cb09a157077a4dca09
Russell Keith-Magee <russell@keith-magee.com>
2014-09-28 13:18:22 +0800

    More tweaks to Travis CI config.

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Office/Business :: Financial :: Accounting',
     ],
     test_suite="tests",
