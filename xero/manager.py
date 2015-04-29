0772e0a2ea89bd6633e437cab904ccc4065acbf0
Tan Thanchirasuk <tan.t@firecreekweb.com>
2015-04-29 12:15:05 +0700

    change check singular conditions in Xero.__init__

--- xero/manager.py
+++ xero/manager.py
@@ -87,7 +87,7 @@ class Manager(object):
 
         # setup our singular variants of the name
         # only if the name ends in 's'
-        if name[-1] == "s":
+        if isplural(name[-1]):
             self.singular = singular(name)
         else:
             self.singular = name
