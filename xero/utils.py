5b37c15c6a41e0af1a1d2c934c69fee8111d9bfc
Robert Wallhead <rwallhead@gmail.com>
2015-07-30 09:43:55 +1000

    Added support for Contact Groups

--- xero/utils.py
+++ xero/utils.py
@@ -16,6 +16,7 @@ OBJECT_NAMES = {
     "BankTransactions": "BankTransaction",
     "BankTransfers": "BankTransfer",
     "BrandingThemes": "BrandingTheme",
+    "ContactGroups": "ContactGroup",
     "Contacts": "Contact",
     "CreditNotes": "CreditNote",
     "Currencies": "Currency",
